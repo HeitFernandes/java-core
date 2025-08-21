aba56169fdd36743e3066c38f05afab6fca33074
Heitor <fernandesheitort2@gmail.com>
2025-08-21 12:25:40 -0300

    fix: Corrigi os Dados Primitvos

--- src/Begginer/DadosPrimitivos/DadosPrimitivos.java
+++ src/Begginer/DadosPrimitivos/DadosPrimitivos.java
@@ -1,4 +1,16 @@
 package Begginer.DadosPrimitivos;
 
+
 public class DadosPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 2147483647;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999999999999999L;
+
+
+
+    }
 }
